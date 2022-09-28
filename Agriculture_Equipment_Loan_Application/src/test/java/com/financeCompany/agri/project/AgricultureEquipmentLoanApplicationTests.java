8ff182e1c2f5e84db63365470e12360e99fc5adc
yash29920 <yashwantkshirsagar342@gmail.com>
2022-09-28 12:11:11 +0530

    Project Added

---
+++ Agriculture_Equipment_Loan_Application/src/test/java/com/financeCompany/agri/project/AgricultureEquipmentLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeCompany.agri.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricultureEquipmentLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
