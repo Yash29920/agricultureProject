ac41c6c2347a785a974b407a181d17f390ca780d
yash29920 <yashwantkshirsagar342@gmail.com>
2022-09-28 16:03:05 +0530

    added by Yash

--- Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/AgricultureEquipmentLoanApplication.java
+++ Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/AgricultureEquipmentLoanApplication.java
@@ -8,6 +8,8 @@ public class AgricultureEquipmentLoanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgricultureEquipmentLoanApplication.class, args);
+
+        System.out.println("finance Api Started");
 	}
 
 }
