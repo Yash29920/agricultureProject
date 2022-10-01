0b8a0a2ead338b28dcbf42f5346c5e39cf6add8c
Mohini-Nikhare <mohininikhare1112@gmail.com>
2022-10-01 15:07:03 +0530

    mohini.........1111

---
+++ Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/appModel/CibilScoreCheck.java
@@ -0,0 +1,33 @@
+package com.financeCompany.agri.project.appModel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CibilScoreCheck {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int customerid;
+	
+	private String firstname;
+	private String lastName;
+	private String email;
+	private String panNo;
+	private long phoneNo;
+	private String gender;
+	private int cibilscore;
+	
+	
+	
+
+}
