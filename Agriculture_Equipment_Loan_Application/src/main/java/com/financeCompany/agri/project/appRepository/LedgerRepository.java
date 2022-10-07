96fb686c657f1896ebc366785d003cf8a1452706
Mohini-Nikhare <mohininikhare1112@gmail.com>
2022-10-07 13:40:52 +0530

    updated by mohini

--- Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/appRepository/LedgerRepository.java
+++ Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/appRepository/LedgerRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.financeCompany.agri.project.appModel.CustomerLedger;
 
 public interface LedgerRepository extends JpaRepository<CustomerLedger, Integer> {
+	
+	
+	public CustomerLedger findByRegcustomerid(int regcustomerid);
 
 }
