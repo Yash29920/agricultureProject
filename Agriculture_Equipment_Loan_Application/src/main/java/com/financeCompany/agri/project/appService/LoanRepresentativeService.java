d24d986372b03c29adda4b3bcbccbe2e35e229f7
madhavdeshmukh <madhavdeshmukh0012@gmail.com>
2022-10-09 22:46:26 +0530

    added by madhav

--- Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/appService/LoanRepresentativeService.java
+++ Agriculture_Equipment_Loan_Application/src/main/java/com/financeCompany/agri/project/appService/LoanRepresentativeService.java
@@ -3,6 +3,7 @@ package com.financeCompany.agri.project.appService;
 import java.util.List;
 
 import com.financeCompany.agri.project.appDto.EnquirySubsidyOfferDto;
+import com.financeCompany.agri.project.appModel.CustomerMailSend;
 import com.financeCompany.agri.project.appModel.EnquiryEmiCalculation;
 import com.financeCompany.agri.project.appModel.SubsidyOffer;
 
@@ -12,5 +13,7 @@ public interface LoanRepresentativeService {
 	String emiUpdate(List<EnquiryEmiCalculation> enquiryEmiCalculation);
 
 	String provideOfferToEnquiry(List<EnquirySubsidyOfferDto> enquirySubsidyOfferDto);
+
+	String sendMailToCustomer(CustomerMailSend mailInfo);
 
 }
